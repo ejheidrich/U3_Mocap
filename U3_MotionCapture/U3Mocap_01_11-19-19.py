6a2e03d25cd5bc69e7b0e574c3bd05b481bd6f69
ejheidrich <57458531+ejheidrich@users.noreply.github.com>
2019-11-19 12:20:39 -0800

    Add files via upload

---
+++ U3_MotionCapture/U3Mocap_01_11-19-19.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 11:49:47 2019
+
+@author: ElsaHeidrich
+
+This code will record an image of the giont of interest from a webcam.
+"""
+
+#import needed libraries - OpenCV (cv2), others?
+
+#captuer one image frame from the camera (take a picture)
+
+#create window to display image
+
+#display the image in the window
+
+#wait for button press
+
+# save the image, and/or destroy windows
+
